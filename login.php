18efab02514a327092c24d5c9d200a9dff04c900
God237 <bantarfavourgwoni@gmail.com>
2026-04-08 05:48:40 +0100

    delete file, edit code

--- login.php
+++ login.php
@@ -150,7 +150,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login | Telemed Connect</title>
-    <link rel="stylesheet" href="login-signup.css">
+    <link rel="stylesheet" href="./login-signup.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     </head>
 <body>
